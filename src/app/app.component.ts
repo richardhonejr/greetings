3cc52ede48ba2f7ef772b35ff7332cf8bbaf44b2
Richard Hone <richard@engineeringrh.com>
2022-03-15 18:40:55 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'greetings';
+  title = 'Angular';
+  studentName = "Dunce Cap"
 }
